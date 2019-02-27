578df6f5c557a3296c2715a01f9b57edc0cb79aa
Harsha Bhargav <hbhargav@users.noreply.github.com>
2019-02-27 17:41:32 +0530

    Update Anagram.java

--- src/main/java/com/talfinder/assessment/Anagram.java
+++ src/main/java/com/talfinder/assessment/Anagram.java
@@ -1,12 +1,27 @@
 package com.talfinder.assessment;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Anagram {
 
-  public List<String> evalAnagrams(String parent, String child) {
+  public static List<String> evalAnagrams(String parent, String child) {
+    /*
+     * NOTE:
+     * 1. Write your solution within this method
+     * 2. Do not make changes to the Class Name, Method Name or Method Signature
+     * 3. Use the Run-Java command to run main method
+     * 4. Use the BuildAndRunTests command to evaluate your solution
+     */
     List<String> anagramsList = null;
     return anagramsList;
+  }
+  
+  public static void main(String[] args) {
+    List<String> anagramsList = evalAnagrams("Harsha", "ash");
+    if(anagramsList != null) {
+        anagramsList.forEach(anagram -> System.out.println(anagram));   
+    } else {
+        System.out.println("List is NULL");
+    }
   }
 }
